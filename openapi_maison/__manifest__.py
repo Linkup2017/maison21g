fd367c23584a6c7b49da58ae0a224057bd07d8b0
marokimjy <kimjy@link-up.co.kr>
2022-12-02 07:01:52 +0900

    [ADD] openapi_maison res.partner, product.product, sale.order, mrp.production 모듈 데이터 추가

---
+++ openapi_maison/__manifest__.py
@@ -0,0 +1,20 @@
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+{
+    "name": """REST API/OpenAPI/Swagger Maison""",
+    "summary": """Data of OpenAPI module customized for Maison""",
+    "category": "",
+    "images": ["images/openapi-swagger.png"],
+    "version": "1.0",
+    "application": False,
+    "author": "LinkUp Info Tech",
+    "support": "",
+    "website": "",
+    "license": "LGPL-3",
+    "depends": ["openapi"],
+    "data": [
+        "data/openapi_data.xml",
+    ],
+    "demo": [],
+    "qweb": [],
+    "installable": True,
+}
